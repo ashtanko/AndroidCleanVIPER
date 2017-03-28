72aaec50acf67049ba6b5e6845a6a76e16a4c404
Alexey <adamluissean@gmail.com>
2017-03-28 11:16:02 +0300

    [WIP] [DEPENDENCIES] Add gradle wrapper + domian layer module

--- app/src/main/kotlin/io/shtanko/viper/presentation/MainActivity.kt
+++ app/src/main/kotlin/io/shtanko/viper/presentation/MainActivity.kt
@@ -1,7 +1,15 @@
 package io.shtanko.viper.presentation
 
+import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import io.shtanko.android_viper.R
 
 class MainActivity : AppCompatActivity() {
 
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+
+    setContentView(R.layout.activity_main)
+  }
 }
